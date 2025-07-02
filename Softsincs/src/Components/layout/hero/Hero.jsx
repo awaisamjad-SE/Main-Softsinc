0451881e489b910289fdc024fe4500415f6a3115
Saad Abbas <2023cs598@student.uet.edu.pk>
2025-07-02 15:06:33 +0500

    update

--- Softsincs/src/Components/layout/hero/Hero.jsx
+++ Softsincs/src/Components/layout/hero/Hero.jsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import HeroButtons from './HeroButtons';
+import React from "react";
+import HeroButtons from "./HeroButtons";
 const Hero = () => {
   return (
-   <section className="pt-[140px] relative h-screen w-full overflow-hidden">
-  {/* Image Background */}
-  <img
-    src="/Services-Images/picture.jpg"
-    alt="Service"
-    className="absolute top-0 left-0 object-cover w-full h-full"
-  />
-
-  {/* Optional content on top */}
-  <div className="relative z-10 text-center text-white">
-    <h1 className="text-5xl font-bold">Welcome to Softsincs</h1>
-    <p className="mt-4 text-lg">Your digital innovation partner.</p>
-  </div>
-
-
-
+    <section className="pt-[140px] relative h-screen w-full overflow-hidden">
+      {/* Image Background */}
+      <img
+        src="/Services-Images/picture.jpg"
+        alt="Service"
+        className="absolute top-0 left-0 object-cover w-full h-full"
+      />
 
       {/* Transparent Overlay Content */}
       <div className="relative z-10 flex items-center justify-center h-full bg-transparent">
