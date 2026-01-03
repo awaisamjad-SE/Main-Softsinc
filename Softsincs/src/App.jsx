6f6748f4e019ad257bd76ca6a6f0e3370fe5e2eb
Saad Abbas <2023cs598@student.uet.edu.pk>
2026-01-03 23:08:49 +0500

    up

--- Softsincs/src/App.jsx
+++ Softsincs/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from './components/layout/navbar/Navbar.jsx';
-import Footer from './components/layout/footer/Footer.jsx';
-import AppRoutes from './routes.jsx';
-import ScrollToTop from './components/common/ScrollToTop.jsx';
-import RouteChangePreloader from './routes/RouteChangePreloader.jsx';
-import ParticleBackground from './components/common/ParticleBackground.jsx';
+import Navbar from './components/layout/navbar/Navbar';
+import Footer from './components/layout/footer/Footer';
+import AppRoutes from './routes';
+import ScrollToTop from './components/common/ScrollToTop';
+import RouteChangePreloader from './routes/RouteChangePreloader';
+import ParticleBackground from './components/common/ParticleBackground';
 
 function App() {
   return (
